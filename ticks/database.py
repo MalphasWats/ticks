eedf388cb7cc5340c465956ccc635f7cbd7589d8
MalphasWats <malphas@subdimension.co.uk>
2013-01-19 22:02:55 +0000

    database schema

--- ticks/database.py
+++ ticks/database.py
@@ -5,7 +5,43 @@ import instruments.database as db
 def create_tables():
 
     schema = """
-        
-    """
+    CREATE TABLE ticks
+    (
+        task_id SERIAL PRIMARY KEY,
+        task_content TEXT NOT NULL,
+        created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        completed timestamp DEFAULT NULL
+    );
+
+    CREATE TABLE ticks_projects
+    (
+        project_id SERIAL PRIMARY KEY,
+        project_name TEXT NOT NULL
+    );
+    CREATE INDEX project_name on ticks_projects(project_name);
+
+    CREATE TABLE project_ticks
+    (
+        project_id INTEGER NOT NULL,
+        task_id INTEGER NOT NULL,
+        PRIMARY KEY (project_id, task_id)
+    );"""
 
     db.execute_query(schema)
+    
+    
+def tables_created():
+    
+    query = """
+    SELECT relname FROM pg_class 
+    WHERE relname = 'ticks'
+    OR relname = 'ticks_projects'
+    OR relname = 'projects_ticks';
+    """
+    
+    result = db.execute_query(query)
+    
+    if result:
+        return True
+        
+    return False
